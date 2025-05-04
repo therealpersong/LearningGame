7362bc85da5efe88166d794114809f0ef77ff6a1
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-05-04 15:08:46 +0100

    did it work?

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
+import time
+import random
 
-print("this is the code")
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Dodge")
+
+print("Hello I anm coding right now is it registering)")
+
+if __name__ == "__main__":
+    pygame.init()
+    main()
+    pygame.quit()
+    print("Game Over")
+    
